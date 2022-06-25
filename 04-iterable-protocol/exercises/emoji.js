8cace7ff7800e20807c9d414d31228244dc57969
Luca Scannapieco <lucascannapieco.94@gmail.com>
2022-06-25 11:28:50 +0200

    implement emoji iterable

--- 04-iterable-protocol/exercises/emoji.js
+++ 04-iterable-protocol/exercises/emoji.js
@@ -23,19 +23,55 @@
 
 export function createEmojiIter (text) {
   // write your code here
+  let index = 0
+  const chars = Array.from(text)
+
+  return {
+    [Symbol.iterator] () {
+      return {
+        next () {
+          while (index < chars.length) {
+            const char = chars[index++]
+            if (char.match(/\p{Emoji}/u) !== null) {
+              return { done: false, value: char }
+            }
+          }
+    
+          return { done: true, value: undefined }
+        }
+      }
+    }
+  }
 }
 
 export class EmojiIter {
   constructor (text) {
     this.chars = Array.from(text)
     // write your code here
+    this.index = 0
   }
 
   next () {
-    // write your code here
+    while (this.index < this.chars.length) {
+      const char = this.chars[this.index++]
+      if (char.match(/\p{Emoji}/u) !== null) {
+        return { done: false, value: char }
+      }
+    }
+
+    return { done: true, value: undefined }
+  }
+
+  [Symbol.iterator] () {
+    return this
   }
 }
 
 export function * emojiIterGen (text) {
   // write your code here
+  for (const char of text) {
+    if (char.match(/\p{Emoji}/u) !== null) {
+      yield char
+    }
+  }
 }
